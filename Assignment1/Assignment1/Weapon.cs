a0aab98f4ffe5ce80de9858d184ffcd1bb5a4212
Narda <153568349+Narda05@users.noreply.github.com>
2026-01-12 17:13:26 -0800

    Add files via upload

---
+++ Assignment1/Assignment1/Weapon.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public class Weapon
+    {
+        // Name,Type,Rarity,BaseAttack
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Rarity { get; set; }
+        public int BaseAttack { get; set; }
+        
+        /// <summary>
+        /// The Comparator function to check for name
+        /// </summary>
+        /// <param name="left">Left side Weapon</param>
+        /// <param name="right">Right side Weapon</param>
+        /// <returns> -1 (or any other negative value) for "less than", 0 for "equals", or 1 (or any other positive value) for "greater than"</returns>
+        public static int CompareByName(Weapon left, Weapon right)
+        {
+            if (ReferenceEquals(left, right)) return 0;
+            if (left is null) return -1;
+            if (right is null) return 1;
+            return string.Compare(left.Name, right.Name, StringComparison.Ordinal);
+        }
+
+        // TODO: add sort for each property:
+        // CompareByType
+        public static int CompareByType(Weapon left, Weapon right)
+        {
+            if (ReferenceEquals(left, right)) return 0;
+            if (left is null) return -1;
+            if (right is null) return 1;
+            return string.Compare(left.Type, right.Type, StringComparison.Ordinal);
+        }
+
+        // CompareByRarity
+        public static int CompareByRarity(Weapon left, Weapon right)
+        {
+            if (ReferenceEquals(left, right)) return 0;
+            if (left is null) return -1;
+            if (right is null) return 1;
+            return Comparer<int>.Default.Compare(left.Rarity, right.Rarity);
+        }
+
+        // CompareByBaseAttack
+        public static int CompareByBaseAttack(Weapon left, Weapon right)
+        {
+            if (ReferenceEquals(left, right)) return 0;
+            if (left is null) return -1;
+            if (right is null) return 1;
+            return Comparer<int>.Default.Compare(left.BaseAttack, right.BaseAttack);
+        }
+
+        /// <summary>
+        /// The Weapon string with all the properties
+        /// </summary>
+        /// <returns>The Weapon formated string</returns>
+        public override string ToString()
+        {
+            // TODO: construct a comma seperated value string
+            // Name,Type,Rarity,BaseAttack
+            return $"{Name},{Type},{Rarity},{BaseAttack}";
+        }
+    }
+}
